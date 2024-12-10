20bd33021d4b452690f28daeca3f050ce88e0a7b
emanzito <eoliveiracorreia417@gmail.com>
2024-12-10 21:56:53 +0000

    correção erros

--- js/form.js
+++ js/form.js
@@ -4,8 +4,8 @@ function form() {
   var coiso = document.getElementById("caixa").value;
   if (coiso === "") {
     window.alert("Por favor, preenche o campo!");
-  } else if (coiso === "A física prova que cair no chão é ciência, não azar.") {
-    window.alert("Parabéns monco, concluíste o desafio!");
+  } else if (coiso === "A física prova que cair no chão é ciência, não azar") {
+    window.alert("Parabéns, concluíste o desafio!");
   } else {
     window.alert("A frase está incorreta!");
   }
